6d121856057ccbbef75918287df24b5d7376c096
Pratik Mahamuni <160023552+Pratik-Mahamuni17@users.noreply.github.com>
2025-10-07 08:41:06 +0530

    Add files via upload

---
+++ async-await.js
@@ -0,0 +1,11 @@
+async function fetchUser() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+    const user = await response.json();
+    console.log(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
+}
+
+fetchUser();
